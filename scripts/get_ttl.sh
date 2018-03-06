e107dc776a215b9ad5101f52251e1d463cc48adf
Tom Elliott <tom.elliott@nyu.edu>
2018-03-06 12:54:33 -0600

    make script robust for execution from any location

--- scripts/get_ttl.sh
+++ scripts/get_ttl.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-cd ./rdf
-pwd
+SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+RDFDIR="$SCRIPTDIR"/../rdf/
+
+cd "$RDFDIR"
 wget http://atlantides.org/downloads/pleiades/rdf/pleiades-latest.tar.gz
 tar -xzf pleiades-latest.tar.gz
 rm *.gz
