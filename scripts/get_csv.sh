8cd5b7be952d7dbcc37c07b3671a00b5545cc181
Tom Elliott <tom.elliott@nyu.edu>
2018-03-06 12:56:12 -0600

    make script more robust: execute from anywhere

--- scripts/get_csv.sh
+++ scripts/get_csv.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-cd ./csv
-pwd
+SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+CSVDIR="$SCRIPTDIR"/../csv/
+
+cd "$CSVDIR"
 wget http://atlantides.org/downloads/pleiades/dumps/pleiades-places-latest.csv.gz http://atlantides.org/downloads/pleiades/dumps/pleiades-locations-latest.csv.gz http://atlantides.org/downloads/pleiades/dumps/pleiades-names-latest.csv.gz
+rm *.csv
 gunzip *.csv.gz
