aa04f48a4584e7aa5c6ea946d50ee5ddf20e543f
Tom Elliott <tom.elliott@nyu.edu>
2018-05-06 07:11:55 -0500

    stop deleting zotero.csv when updating place, name, and location CSV files

--- scripts/get_csv.sh
+++ scripts/get_csv.sh
@@ -5,5 +5,5 @@ CSVDIR="$SCRIPTDIR"/../csv/
 
 cd "$CSVDIR"
 wget http://atlantides.org/downloads/pleiades/dumps/pleiades-places-latest.csv.gz http://atlantides.org/downloads/pleiades/dumps/pleiades-locations-latest.csv.gz http://atlantides.org/downloads/pleiades/dumps/pleiades-names-latest.csv.gz
-rm *.csv
+rm pleiades-*.csv
 gunzip *.csv.gz
