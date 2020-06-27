9c90a7f7523853ec29faea49b4b81e52d00f8ef2
Tom Elliott <tom.elliott@nyu.edu>
2020-06-27 15:52:25 -0500

    restructuring for package

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pleiades.datasets",
+    version="2.0",
+    author="Tom Elliott",
+    author_email="tom.elliott@nyu.edu",
+    description="Platform-independent data from the Pleiades gazetteer",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://pleiades.stoa.org",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=['airtight'],
+    python_requires='>=3.8.3'
+)
