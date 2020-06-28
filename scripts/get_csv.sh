35a7f7e28f08e3b6b491aeab93d637eaea4e57d0
Tom Elliott <tom.elliott@nyu.edu>
2020-06-28 06:53:49 -0500

    adjust paths for new data directory

--- scripts/get_csv.sh
+++ scripts/get_csv.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-CSVDIR="$SCRIPTDIR"/../csv/
+CSVDIR="$SCRIPTDIR"/../data/csv/
 
 cd "$CSVDIR"
 wget http://atlantides.org/downloads/pleiades/dumps/pleiades-places-latest.csv.gz http://atlantides.org/downloads/pleiades/dumps/pleiades-locations-latest.csv.gz http://atlantides.org/downloads/pleiades/dumps/pleiades-names-latest.csv.gz
