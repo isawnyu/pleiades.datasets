fb5482eab97ad329bb38a99881852dd8060bf899
Pleiades Administrator <pleiades.admin@nyu.edu>
2020-08-15 06:31:13 -0500

    updated csv

--- scripts/cron.sh
+++ scripts/cron.sh
@@ -10,7 +10,16 @@ PD_HOME=/home/paregorios/Documents/files/P/pleiades.datasets
 # su paregorios
 . /home/paregorios/.profile
 cd $PD_HOME
+git checkout main
 ./.direnv/python-3.8.3/bin/python scripts/get_json.py
 bash ./scripts/get_csv.sh
 bash ./scripts/get_ttl.sh
+git add data/csv
+git commit -m 'updated csv'
+git add data/json
+git commit -m 'updated json'
+git add data/rdf
+git commit -m 'updated rdf/ttl'
+git push origin main
+
 
