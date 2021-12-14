eea2dd050d10c248e42c683896e124d57c106155
Tom Elliott <ipse@paregorios.org>
2021-12-14 09:31:08 -0600

    script to automate full update of the package data

---
+++ scripts/update.sh
@@ -0,0 +1,20 @@
+#! /usr/local/bin/bash
+set -x
+
+PD_HOME=/Users/paregorios/Documents/files/P/pleiades.datasets
+
+cd $PD_HOME
+nice git checkout main
+nice git pull origin main
+nice $VIRTUAL_ENV/bin/python $PD_HOME/scripts/get_json.py -x
+nice bash ./scripts/get_csv.sh
+nice bash ./scripts/get_ttl.sh
+nice git add data/csv
+nice git commit -m 'updated csv'
+nice git add data/json
+nice git commit -m 'updated json'
+nice git add data/rdf
+nice git commit -m 'updated rdf/ttl'
+nice git push origin main
+
+
