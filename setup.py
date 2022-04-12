bfd72b4a568ef3b83e7ed31d0a4975e93f3416dc
Tom Elliott <ipse@paregorios.org>
2022-04-11 19:02:21 -0500

    add shapely to dependencies

--- setup.py
+++ setup.py
@@ -18,6 +18,6 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    install_requires=['airtight', 'python-dateutil', 'requests'],
-    python_requires='>=3.8.3'
+    install_requires=["airtight", "python-dateutil", "requests", "shapely"],
+    python_requires=">=3.8.3",
 )
