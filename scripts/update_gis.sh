610dd03f1229b1fc6ed163316f05713b701f7f15
Tom Elliott <ipse@paregorios.org>
2023-10-02 09:12:44 -0500

    fix pleiades-gazetteer/issues/#503: ensure zip file uses relative paths

--- scripts/update_gis.sh
+++ scripts/update_gis.sh
@@ -11,7 +11,8 @@ nice $VIRTUAL_ENV/bin/python $PD_HOME/scripts/update_gis.py
 nice git add data/gis
 nice git commit -m 'updated gis package'
 nice git push origin main
-nice zip $HOME/scratch/pleiades_gis_data.zip $PD_HOME/data/gis/*.csv $PD_HOME/data/gis/README.md $PD_HOME/data/gis/index.html
+rm $HOME/scratch/pleiades_gis_data.zip
+nice zip $HOME/scratch/pleiades_gis_data.zip ./data/gis/*.csv ./data/gis/README.md ./data/gis/index.html
 nice scp $HOME/scratch/pleiades_gis_data.zip isaw1:pleiades_gis/
 nice scp $PD_HOME/data/gis/index.html isaw1:pleiades_gis/
 echo "**** MANUAL STEP: ssh into isaw1, su to plone_daemon, and copy the files in ~/pleiades_gis/ to /var/www/atlantides.org/downloads/pleiades/gis/"
