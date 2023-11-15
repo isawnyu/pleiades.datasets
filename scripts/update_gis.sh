416aba1681f5cc328e2d7c74a3f4ecc22b3204d5
Tom Elliott <ipse@paregorios.org>
2023-11-15 08:26:33 -0600

    remove manual step in gis package upload

--- scripts/update_gis.sh
+++ scripts/update_gis.sh
@@ -13,7 +13,9 @@ nice git commit -m 'updated gis package'
 nice git push origin main
 rm $HOME/scratch/pleiades_gis_data.zip
 nice zip $HOME/scratch/pleiades_gis_data.zip ./data/gis/*.csv ./data/gis/README.md ./data/gis/index.html
-nice scp $HOME/scratch/pleiades_gis_data.zip isaw1:pleiades_gis/
-nice scp $PD_HOME/data/gis/index.html isaw1:pleiades_gis/
-echo "**** MANUAL STEP: ssh into isaw1, su to plone_daemon, and copy the files in ~/pleiades_gis/ to /var/www/atlantides.org/downloads/pleiades/gis/"
+# nice scp $HOME/scratch/pleiades_gis_data.zip isaw1:pleiades_gis/
+rsync --rsync-path="sudo rsync" --compress --progress --update --times --perms --chmod=Du=rwx,Dg=rx,Do=rx,Fu=rw,Fg=rw,Fo=r --owner --group --chown=www-data:plone_group --partial-dir=.rsync_partial $HOME/scratch/pleiades_gis_data.zip isaw1:/var/www/atlantides.org/downloads/pleiades/gis/
+#nice scp $PD_HOME/data/gis/index.html isaw1:pleiades_gis/
+rsync --rsync-path="sudo rsync" --compress --progress --update --times --perms --chmod=Du=rwx,Dg=rx,Do=rx,Fu=rw,Fg=rw,Fo=r --owner --group --chown=www-data:plone_group --partial-dir=.rsync_partial $PD_HOME/data/gis/index.html isaw1:/var/www/atlantides.org/downloads/pleiades/gis/
+# echo "**** MANUAL STEP: ssh into isaw1, su to plone_daemon, and copy the files in ~/pleiades_gis/ to /var/www/atlantides.org/downloads/pleiades/gis/"
 
