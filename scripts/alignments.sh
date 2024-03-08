af673e04ff042130a7651791bb78a827aa9314a0
Tom Elliott <ipse@paregorios.org>
2024-03-08 06:06:23 -0600

    modified alignments script to store output in data/indexes

--- scripts/alignments.sh
+++ scripts/alignments.sh
@@ -7,28 +7,28 @@ cd $SCRIPT_DIR/..
 PD_HOME=$PWD
 
 # manto
-nice $VIRTUAL_ENV/bin/python $PD_HOME/scripts/alignments.py -z SLCUI2TB -f json > $PD_HOME/data/alignments/manto.json
-nice $VIRTUAL_ENV/bin/python $PD_HOME/scripts/alignments.py -z SLCUI2TB -f markdown -g zotkey > $PD_HOME/data/alignments/manto.md
+nice $VIRTUAL_ENV/bin/python $PD_HOME/scripts/alignments.py -z SLCUI2TB -f json > $PD_HOME/data/indexes/manto.json
+nice $VIRTUAL_ENV/bin/python $PD_HOME/scripts/alignments.py -z SLCUI2TB -f markdown -g zotkey > $PD_HOME/data/indexes/manto.md
 
 # cfl/ago
-nice $VIRTUAL_ENV/bin/python $PD_HOME/scripts/alignments.py -z 9KVQ7A64 -f json > $PD_HOME/data/alignments/cfl_ago.json
-nice $VIRTUAL_ENV/bin/python $PD_HOME/scripts/alignments.py -z 9KVQ7A64 -f markdown -g zotkey > $PD_HOME/data/alignments/cfl_ago.md
+nice $VIRTUAL_ENV/bin/python $PD_HOME/scripts/alignments.py -z 9KVQ7A64 -f json > $PD_HOME/data/indexes/cfl_ago.json
+nice $VIRTUAL_ENV/bin/python $PD_HOME/scripts/alignments.py -z 9KVQ7A64 -f markdown -g zotkey > $PD_HOME/data/indexes/cfl_ago.md
 
 # topostext
-nice $VIRTUAL_ENV/bin/python $PD_HOME/scripts/alignments.py -z MC9RGDVB -f json > $PD_HOME/data/alignments/topostext.json
-nice $VIRTUAL_ENV/bin/python $PD_HOME/scripts/alignments.py -z MC9RGDVB -f markdown -g zotkey > $PD_HOME/data/alignments/topostext.md
+nice $VIRTUAL_ENV/bin/python $PD_HOME/scripts/alignments.py -z MC9RGDVB -f json > $PD_HOME/data/indexes/topostext.json
+nice $VIRTUAL_ENV/bin/python $PD_HOME/scripts/alignments.py -z MC9RGDVB -f markdown -g zotkey > $PD_HOME/data/indexes/topostext.md
 
 # aio
 # https://www.zotero.org/groups/2533/items/ZPCURT27
-nice $VIRTUAL_ENV/bin/python $PD_HOME/scripts/alignments.py -z ZPCURT27 -f json > $PD_HOME/data/alignments/aio.json
-nice $VIRTUAL_ENV/bin/python $PD_HOME/scripts/alignments.py -z ZPCURT27 -f markdown -g zotkey > $PD_HOME/data/alignments/aio.md
+nice $VIRTUAL_ENV/bin/python $PD_HOME/scripts/alignments.py -z ZPCURT27 -f json > $PD_HOME/data/indexes/aio.json
+nice $VIRTUAL_ENV/bin/python $PD_HOME/scripts/alignments.py -z ZPCURT27 -f markdown -g zotkey > $PD_HOME/data/indexes/aio.md
 
 # wikidata
 # https://www.zotero.org/groups/2533/items/BCQIKDKS
-nice $VIRTUAL_ENV/bin/python $PD_HOME/scripts/alignments.py -z BCQIKDKS -f json > $PD_HOME/data/alignments/wikidata.json
-nice $VIRTUAL_ENV/bin/python $PD_HOME/scripts/alignments.py -z BCQIKDKS -f markdown -g zotkey > $PD_HOME/data/alignments/wikidata.md
+nice $VIRTUAL_ENV/bin/python $PD_HOME/scripts/alignments.py -z BCQIKDKS -f json > $PD_HOME/data/indexes/wikidata.json
+nice $VIRTUAL_ENV/bin/python $PD_HOME/scripts/alignments.py -z BCQIKDKS -f markdown -g zotkey > $PD_HOME/data/indexes/wikidata.md
 
 # trismegistos
 # https://www.zotero.org/groups/2533/items/TN3GJAU8
-nice $VIRTUAL_ENV/bin/python $PD_HOME/scripts/alignments.py -z TN3GJAU8 -f json > $PD_HOME/data/alignments/tm.json
-nice $VIRTUAL_ENV/bin/python $PD_HOME/scripts/alignments.py -z TN3GJAU8 -f markdown -g zotkey > $PD_HOME/data/alignments/tm.md
+nice $VIRTUAL_ENV/bin/python $PD_HOME/scripts/alignments.py -z TN3GJAU8 -f json > $PD_HOME/data/indexes/tm.json
+nice $VIRTUAL_ENV/bin/python $PD_HOME/scripts/alignments.py -z TN3GJAU8 -f markdown -g zotkey > $PD_HOME/data/indexes/tm.md
