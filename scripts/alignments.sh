ed36f22c8a10daccca3da8d901abb4214e7f598f
Tom Elliott <ipse@paregorios.org>
2024-03-08 06:02:02 -0600

    add TM to alignements script

--- scripts/alignments.sh
+++ scripts/alignments.sh
@@ -14,20 +14,21 @@ nice $VIRTUAL_ENV/bin/python $PD_HOME/scripts/alignments.py -z SLCUI2TB -f markd
 nice $VIRTUAL_ENV/bin/python $PD_HOME/scripts/alignments.py -z 9KVQ7A64 -f json > $PD_HOME/data/alignments/cfl_ago.json
 nice $VIRTUAL_ENV/bin/python $PD_HOME/scripts/alignments.py -z 9KVQ7A64 -f markdown -g zotkey > $PD_HOME/data/alignments/cfl_ago.md
 
-
 # topostext
 nice $VIRTUAL_ENV/bin/python $PD_HOME/scripts/alignments.py -z MC9RGDVB -f json > $PD_HOME/data/alignments/topostext.json
 nice $VIRTUAL_ENV/bin/python $PD_HOME/scripts/alignments.py -z MC9RGDVB -f markdown -g zotkey > $PD_HOME/data/alignments/topostext.md
-
 
 # aio
 # https://www.zotero.org/groups/2533/items/ZPCURT27
 nice $VIRTUAL_ENV/bin/python $PD_HOME/scripts/alignments.py -z ZPCURT27 -f json > $PD_HOME/data/alignments/aio.json
 nice $VIRTUAL_ENV/bin/python $PD_HOME/scripts/alignments.py -z ZPCURT27 -f markdown -g zotkey > $PD_HOME/data/alignments/aio.md
 
-
 # wikidata
 # https://www.zotero.org/groups/2533/items/BCQIKDKS
 nice $VIRTUAL_ENV/bin/python $PD_HOME/scripts/alignments.py -z BCQIKDKS -f json > $PD_HOME/data/alignments/wikidata.json
 nice $VIRTUAL_ENV/bin/python $PD_HOME/scripts/alignments.py -z BCQIKDKS -f markdown -g zotkey > $PD_HOME/data/alignments/wikidata.md
 
+# trismegistos
+# https://www.zotero.org/groups/2533/items/TN3GJAU8
+nice $VIRTUAL_ENV/bin/python $PD_HOME/scripts/alignments.py -z TN3GJAU8 -f json > $PD_HOME/data/alignments/tm.json
+nice $VIRTUAL_ENV/bin/python $PD_HOME/scripts/alignments.py -z TN3GJAU8 -f markdown -g zotkey > $PD_HOME/data/alignments/tm.md
