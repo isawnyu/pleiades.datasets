3ecf9630bf0ff6b570da8310d386073e8278bf3e
Tom Elliott <ipse@paregorios.org>
2024-10-15 11:04:48 -0400

    make ttl download script more robust

--- scripts/get_ttl.sh
+++ scripts/get_ttl.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
+set -e 
+set -o pipefail
+set -u
 
 SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 RDFDIR="$SCRIPTDIR"/../data/rdf/
 
 cd "$RDFDIR"
+rm -f *.gz
+rm -f *.gz.*
 wget http://atlantides.org/downloads/pleiades/rdf/pleiades-latest.tar.gz
 tar -xzf pleiades-latest.tar.gz
-rm *.gz
-rm *.gz.?
+rm -f *.gz
+rm -f *.gz.*
