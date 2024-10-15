8ee23a03c383e73c5765573d96ef56945be550f0
Tom Elliott <ipse@paregorios.org>
2024-10-15 11:43:47 -0400

    further script adjustments

--- scripts/update.sh
+++ scripts/update.sh
@@ -11,22 +11,28 @@ PD_HOME=$PWD
 git checkout main
 git pull origin main
 
-echo "\n\nDownloading and splitting JSON into individual files\n=================================================\n"
+printf "\n\nDownloading and splitting JSON into individual files\n=================================================\n"
 $VIRTUAL_ENV/bin/python $PD_HOME/scripts/get_json.py -r
 git add data/json
+set +e
 git commit -m 'updated json'
+set -e
 
-echo "\n\nDownloading legacy CSV\n=================================================\n"
+printf "\n\nDownloading legacy CSV\n=================================================\n"
 bash ./scripts/get_csv.sh
 git add data/csv/*.csv
-git commit -m 'updated csv'
+set +e
+git commit -m 'updated csv' 2> /dev/null
+set -e
 
-echo "\n\nDownloading RDF/TTL\n=================================================\n"
+printf "\n\nDownloading RDF/TTL\n=================================================\n"
 bash ./scripts/get_ttl.sh
 git add data/rdf
-git commit -m 'updated rdf/ttl'
+set +e
+git commit -m 'updated rdf/ttl' 2> /dev/null
+set -e
 
-echo "\n\nPushing changes to GitHub\n=================================================\n"
+printf "\n\nPushing changes to GitHub\n=================================================\n"
 git push origin main
 
 
