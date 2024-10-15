ecd5d8948575467fbeab165ecc6a7ff4506c871e
Tom Elliott <ipse@paregorios.org>
2024-10-15 11:34:39 -0400

    make update script more robust

--- scripts/update.sh
+++ scripts/update.sh
@@ -10,15 +10,23 @@ PD_HOME=$PWD
 
 git checkout main
 git pull origin main
+
+echo "\n\nDownloading and splitting JSON into individual files\n=================================================\n"
 $VIRTUAL_ENV/bin/python $PD_HOME/scripts/get_json.py -r
-bash ./scripts/get_csv.sh
-bash ./scripts/get_ttl.sh
-git add data/csv/*.csv
-git commit -m 'updated csv'
 git add data/json
 git commit -m 'updated json'
+
+echo "\n\nDownloading legacy CSV\n=================================================\n"
+bash ./scripts/get_csv.sh
+git add data/csv/*.csv
+git commit -m 'updated csv'
+
+echo "\n\nDownloading RDF/TTL\n=================================================\n"
+bash ./scripts/get_ttl.sh
 git add data/rdf
 git commit -m 'updated rdf/ttl'
+
+echo "\n\nPushing changes to GitHub\n=================================================\n"
 git push origin main
 
 
