925efbc30dd3448d3089dd7b3a43f00be60e8cce
Tom Elliott <ipse@paregorios.org>
2024-10-15 10:00:55 -0400

    add cleanup steps to CSV download/decompress script

--- scripts/get_csv.sh
+++ scripts/get_csv.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
+set -e 
+set -o pipefail
+set -u
 
 SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 CSVDIR="$SCRIPTDIR"/../data/csv/
 
 cd "$CSVDIR"
-wget http://atlantides.org/downloads/pleiades/dumps/pleiades-places-latest.csv.gz http://atlantides.org/downloads/pleiades/dumps/pleiades-locations-latest.csv.gz http://atlantides.org/downloads/pleiades/dumps/pleiades-names-latest.csv.gz
+rm -f *.gz
+rm -f *.gz.*
+wget http://atlantides.org/downloads/pleiades/dumps/pleiades-places-latest.csv.gz
+wget http://atlantides.org/downloads/pleiades/dumps/pleiades-locations-latest.csv.gz
+wget http://atlantides.org/downloads/pleiades/dumps/pleiades-names-latest.csv.gz
 rm pleiades-*.csv
 gunzip *.csv.gz
+rm -f *.gz
+rm -f *.gz.*
