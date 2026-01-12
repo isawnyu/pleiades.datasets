0cc3df7c94d8bdb4efc5e6e2038ac20dccbff666
Tom Elliott <ipse@paregorios.org>
2026-01-12 14:08:44 -0600

    checkout and pull

--- scripts/orchestrate.sh
+++ scripts/orchestrate.sh
@@ -3,15 +3,36 @@
 # orchestrate.sh
 # manage the entire daily pleiades.datasets generation routine, including error handling
 
-# set/unset shell attributes
+# set/unset shell attributes and other config
 set -x # Print a trace of simple commands
 set -e # Exit immediately if a pipeline returns a non-zero status
 set -o pipefail # The return value of a pipeline is the value of the last (rightmost) command to exit with a non-zero status, or zero if all commands in the pipeline exit successfully
+set -u
+
+set +x
+SCRIPT_DIR=$(dirname "${BASH_SOURCE[0]}")
+cd $SCRIPT_DIR/..
+PD_HOME=$PWD
+echo "Config:"
+echo "PD_HOME: $PD_HOME"
+echo "VIRTUAL_ENV: $VIRTUAL_ENV"
+
+# ensure clean and up-to-date git working copy
+set -x
+git checkout main
+git pull origin main
 
 # download and archive exports
-
+set +x
+printf "\n\nDownloading and splitting JSON into individual files\n=====================================================\n"
+set -x
+$VIRTUAL_ENV/bin/python $PD_HOME/scripts/get_json.py -r
 
 # check integrity of downloaded json (prerequisite for following steps)
+set +x
+printf "\n\nChecking integrity of downloaded data (have there been retractions?)\n=====================================================\n"
+set -x
+$VIRTUAL_ENV/bin/python $PD_HOME/scripts/check_integrity.py
 
 # FORK 1 STARTS HERE
     # BRANCH 1.A 
