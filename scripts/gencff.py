9cb1fd27fffadec68cd314315e52254a6fa5f6b7
Tom Elliott <ipse@paregorios.org>
2026-01-14 15:22:02 -0600

    stub script to generate CFF

---
+++ scripts/gencff.py
@@ -0,0 +1,45 @@
+"""
+Summarize recent commits
+"""
+
+from airtight.cli import configure_commandline
+import logging
+from pprint import pprint
+
+logger = logging.getLogger(__name__)
+
+
+DEFAULT_LOG_LEVEL = logging.WARNING
+OPTIONAL_ARGUMENTS = [
+    [
+        "-l",
+        "--loglevel",
+        "NOTSET",
+        "desired logging level ("
+        + "case-insensitive string: DEBUG, INFO, WARNING, or ERROR",
+        False,
+    ],
+    ["-v", "--verbose", False, "verbose output (logging level == INFO)", False],
+    [
+        "-w",
+        "--veryverbose",
+        False,
+        "very verbose output (logging level == DEBUG)",
+        False,
+    ],
+]
+POSITIONAL_ARGUMENTS = [
+    # each row is a list with 3 elements: name, type, help
+]
+
+
+def main(**kwargs):
+    pass
+
+
+if __name__ == "__main__":
+    main(
+        **configure_commandline(
+            OPTIONAL_ARGUMENTS, POSITIONAL_ARGUMENTS, DEFAULT_LOG_LEVEL
+        )
+    )
